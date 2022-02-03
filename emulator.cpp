696f68b044eddbb74239299b157ef58a401037fe
Cole <33649427+colehanson17@users.noreply.github.com>
2022-02-03 09:43:05 -0700

    emulator documentation and reformatting

--- emulator.cpp
+++ emulator.cpp
@@ -1,9 +1,20 @@
 #include "include/emulator.h"
 
 void Emulator::init(uint8_t mem_size, FILE *bin_file) {
+  // Initialize memory and print starting instruction pointer
   uint8_t *eip = memory.init(mem_size);
   printf("eip: %x\n", eip);
+
+  // Load binary file into memory allocation
   memory.load_asm_file(bin_file);
-  uint8_t m = memory.read_immd16(*eip);
+
+  // Print first instructions (for testing)
+  uint8_t m = memory.read_byte(*eip);
+  printf("next: %d, %x", m, m);
+  (*eip)++;
+  m = memory.read_byte(*eip);
+  printf("next: %d, %x", m, m);
+  (*eip)++;
+  m = memory.read_byte(*eip);
   printf("next: %d, %x", m, m);
 }
