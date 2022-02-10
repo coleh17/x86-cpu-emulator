c9ec3a686a46e4d8dd31cce8a31f2a1dcd3b3fa1
Cole <33649427+colehanson17@users.noreply.github.com>
2022-02-09 21:07:33 -0700

    started on instructions

--- include/instructions.h
+++ include/instructions.h
@@ -2,5 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "memory.h"
+#include "registers.h"
+
 class Instructions {
+  Memory memory;
+  Registers registers;
+  uint8_t eip;
+
+  void init();
+  void read_instruction(uint8_t instruction);
+
+  /**
+   * Load an immediate of 8 bits to specified register
+   * @param reg Register to load immd to
+   * @param imm8 Immediate to load
+   */
+  void load_immd8_to_reg(uint8_t reg, uint8_t immd8);
 };
--- instructions.cpp
+++ instructions.cpp
@@ -1,1 +1,39 @@
-;
+#include "include/instructions.h"
+
+void Instructions::init() {
+  this->eip = 0;
+  this->memory.init(1024);
+  this->registers.init(this->eip);
+}
+
+void Instructions::read_instruction(uint8_t instruction) {
+}
+
+void Instructions::load_immd8_to_reg(uint8_t reg, uint8_t immd8) {
+  switch (reg) {
+    case 0:
+      this->registers.set_eax(immd8);
+      break;
+    case 1:
+      this->registers.set_ecx(immd8);
+      break;
+    case 2:
+      this->registers.set_edx(immd8);
+      break;
+    case 3:
+      this->registers.set_ebx(immd8);
+      break;
+    case 4:
+      this->registers.set_esp(immd8);
+      break;
+    case 5:
+      this->registers.set_ebp(immd8);
+      break;
+    case 6:
+      this->registers.set_esi(immd8);
+      break;
+    case 7:
+      this->registers.set_edi(immd8);
+      break;
+  }
+}
